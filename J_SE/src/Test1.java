f04e1f882a88cde39f7aa0128b0bd317ffba7515
rocky-zheng <425722309@qq.com>
2018-11-27 14:04:59 +0800

    update main method

--- J_SE/src/Test1.java
+++ J_SE/src/Test1.java
@@ -1,6 +1,7 @@
 
 public class Test1 {
     public static void main(String[] args){
-        System.out.println("Testing");
+        System.out.println("Testing1");
+        System.out.println("Testing2");
     }
 }
