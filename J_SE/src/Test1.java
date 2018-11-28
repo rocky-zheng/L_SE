700b26b431ab6da442252bf05c23082644585312
rocky-zheng <425722309@qq.com>
2018-11-28 13:05:18 +0800

    reflesh Test1

--- J_SE/src/Test1.java
+++ J_SE/src/Test1.java
@@ -6,7 +6,5 @@
 public class Test1 {
     public static void main(String[] args){
         System.out.println("Testing1");
-        System.out.println("Testing2");
-        System.out.println("dev:updated");
     }
 }
