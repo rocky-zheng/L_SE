4807be1b29ca190188e632d4c7525d6bfbf34538
rocky-zheng <425722309@qq.com>
2018-11-28 12:58:02 +0800

    update main method(fixed conflict)

--- J_SE/src/Test1.java
+++ J_SE/src/Test1.java
@@ -1,10 +1,12 @@
 
 /**
  * version1.0.1
+ *
  */
 public class Test1 {
     public static void main(String[] args){
         System.out.println("Testing1");
         System.out.println("Testing2");
+        System.out.println("dev:updated");
     }
 }
