760bf603a61628ae7f42f4bc6e5b7acd2e5d6329
rocky-zheng <425722309@qq.com>
2018-11-28 13:12:50 +0800

    master:insert:Test1

--- J_SE/src/Test1.java
+++ J_SE/src/Test1.java
@@ -6,6 +6,7 @@
 public class Test1 {
     public static void main(String[] args){
         System.out.println("Testing1");
+        System.out.println("master:insert a line.");
         System.out.println("dev1:add a line.");
     }
 }
